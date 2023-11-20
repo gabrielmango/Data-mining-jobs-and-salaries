b1d537a1994def8b854e06a2e967ba18c7441165
Gabriel Mango <gabrielhmango@gmail.com>
2023-11-20 13:03:25 -0300

    feat: getting info links

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -17,9 +18,9 @@ driver.get(URL)
 time.sleep(2)
 
 
-def wait_to_load_element(value, type='xpath'):
+def wait_to_load_element(value, type='xpath', timeout=1):
     while True:
-        time.sleep(0.5)
+        time.sleep(timeout)
         if type == 'xpath' and driver.find_element(By.XPATH, value):
             return driver.find_element(By.XPATH, value)
         elif type == 'class' and driver.find_element(By.CLASS_NAME, value):
@@ -36,12 +37,26 @@ select.select_by_value('100')
 # Find last page number
 last_page_number = int(wait_to_load_element('//*[@id="datatable_paginate"]/ul/li[7]/a', 'xpath').text)
 
+list_of_informations = []
+
 # Make pagination
 for index in range(2, last_page_number + 1):
-    _paginations = wait_to_load_element('pagination', 'class')
+
+    group_rows = wait_to_load_element('//*[@id="datatable"]/tbody', 'xpath')
+    rows = group_rows.find_elements(By.TAG_NAME, 'tr')
+
+    for row in rows:
+        list_of_informations.append(row.find_element(By.TAG_NAME, 'a').get_attribute('href'))
+
+
+    _paginations = wait_to_load_element('pagination', 'class', 2)
     pages = _paginations.find_elements(By.TAG_NAME, 'a')
     for page in pages:
         if page.text == str(index):
+
+            cookie_consent = driver.find_element(By.ID, 'cookieConsent')
+            driver.execute_script("arguments[0].style.display='none';", cookie_consent)
+
             page.click()
             time.sleep(1)
             break
