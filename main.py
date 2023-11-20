5995201ea7365e099f1004747481b53053a8dedd
Gabriel Mango <gabrielhmango@gmail.com>
2023-11-20 10:57:03 -0300

    feat: Set ‘page results’ to 100

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -12,6 +14,8 @@ driver = webdriver.Chrome(service=service)
 
 URL = 'https://www.mgs.srv.br/empregados-por-nomes'
 driver.get(URL)
+time.sleep(2)
+
 
 def wait_to_load(value, element=True):
     while True:
@@ -19,3 +23,9 @@ def wait_to_load(value, element=True):
             return driver.find_element(By.XPATH, value)
         elif not element and driver.find_elements(By.XPATH, value):
             return driver.find_elements(By.XPATH, value)
+        time.sleep(0.5)
+
+# Set ‘page results’ to 100
+pagination = wait_to_load('//*[@id="datatable_length"]/label/select')
+select = Select(pagination)
+select.select_by_value('100')
