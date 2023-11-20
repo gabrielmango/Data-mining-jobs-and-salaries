7f8e880441061f1ace6167688bf79abce55646d1
Gabriel Mango <gabrielhmango@gmail.com>
2023-11-20 12:11:04 -0300

    refactor: funs wait_to_load

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 service = ChromeService(ChromeDriverManager().install())
 options = webdriver.ChromeOptions()
 options.add_argument('--headless=new')
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=service, options=options)
 
 
 URL = 'https://www.mgs.srv.br/empregados-por-nomes'
@@ -17,15 +17,29 @@ driver.get(URL)
 time.sleep(2)
 
 
-def wait_to_load(value, element=True):
+def wait_to_load_element(value, type='xpath'):
     while True:
-        if element and driver.find_element(By.XPATH, value):
+        time.sleep(1)
+        if type == 'xpath' and driver.find_element(By.XPATH, value):
             return driver.find_element(By.XPATH, value)
-        elif not element and driver.find_elements(By.XPATH, value):
-            return driver.find_elements(By.XPATH, value)
-        time.sleep(0.5)
+        elif type == 'class' and driver.find_element(By.CLASS_NAME, value):
+            return driver.find_element(By.CLASS_NAME, value)
+        elif type == 'tag' and driver.find_element(By.TAG_NAME, value):
+            return driver.find_element(By.TAG_NAME, value)
+        
 
+def wait_to_load_elements(value, type='xpath'):
+    while True:
+        time.sleep(1)
+        if type == 'xpath' and driver.find_elements(By.XPATH, value):
+            return driver.find_elements(By.XPATH, value)
+        elif type == 'class' and driver.find_elements(By.CLASS_NAME, value):
+            return driver.find_elements(By.CLASS_NAME, value)
+        elif type == 'tag' and driver.find_elements(By.TAG_NAME, value):
+            return driver.find_elements(By.TAG_NAME, value)
+
+               
 # Set ‘page results’ to 100
-pagination = wait_to_load('//*[@id="datatable_length"]/label/select')
+pagination = wait_to_load_element('//*[@id="datatable_length"]/label/select')
 select = Select(pagination)
 select.select_by_value('100')
